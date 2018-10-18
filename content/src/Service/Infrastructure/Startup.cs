159eac18308d45b4fd862bd22d39e4a0ae1bd430
Bastian Eicher <bastian.eicher@axoom.com>
2018-10-18 10:59:40 +0200

    Improved logging

--- content/src/Service/Infrastructure/Startup.cs
+++ content/src/Service/Infrastructure/Startup.cs
@@ -1,7 +1,9 @@
 using System;
+using Axoom.Extensions.Logging.Console;
 using Axoom.Extensions.Prometheus.Standalone;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace MyVendor.MyService.Infrastructure
 {
@@ -10,13 +12,18 @@ namespace MyVendor.MyService.Infrastructure
         public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
             => services.AddSingleton(configuration)
                        .AddOptions()
-                       .AddAxoomLogging(configuration)
+                       .AddLogging(builder => builder.AddConfiguration(configuration.GetSection("Logging"))
+                                                     .AddExceptionDemystifyer())
                        .AddPrometheusServer(configuration)
                        .AddPolicies(configuration);
 
         public static IServiceProvider UseInfrastructure(this IServiceProvider provider)
         {
-            provider.UseAxoomLogging();
+            provider.GetRequiredService<ILoggerFactory>()
+                    .AddAxoomConsole(provider.GetRequiredService<IConfiguration>().GetSection("Logging"))
+                    .CreateLogger("Startup")
+                    .LogInformation("Starting My Service");
+
             provider.UsePrometheusServer();
 
             return provider;
