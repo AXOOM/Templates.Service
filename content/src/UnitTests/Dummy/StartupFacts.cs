eb8639c56b0215c13d19473697735a6248ae8c61
Bastian Eicher <bastian.eicher@axoom.com>
2018-10-26 11:08:34 +0200

    Added Xunit logger

--- content/src/UnitTests/Dummy/StartupFacts.cs
+++ content/src/UnitTests/Dummy/StartupFacts.cs
@@ -3,11 +3,16 @@ using FluentAssertions;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace MyVendor.MyService.Dummy
 {
     public class StartupFacts : StartupFactsBase
     {
+        public StartupFacts(ITestOutputHelper output)
+            : base(output)
+        {}
+
         [Fact]
         public void CanResolveDummyWorker()
         {
